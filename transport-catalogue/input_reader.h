c0e2fd56b878c4d2d4aa4faaa803e4720e876c2d
Ivanatom90 <115657086+Ivanatom90@users.noreply.github.com>
2023-05-11 00:49:47 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,1 +1,30 @@
-// место для вашего кода
+#pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+#include <ostream>
+#include <sstream>
+#include <istream>
+#include <map>
+
+struct  Request
+{
+    std::string type;
+    std::string request;
+
+};
+
+class RequestBase{
+public:
+//    RequestBase(std::pair<std::vector<Request>, std::vector<Request>>);
+    std::vector<Request> requests_stops; //точки остановки
+    std::vector<Request> bus_stops; //маршруты автобусов
+
+    std::vector<Request>* GetStops();
+    std::vector<Request>* GetBusStops();
+    private:
+};
+
+Request ReadInput(std::string& str);
+int RequestNumber(std::string& str);
+RequestBase GetRequestBase(std::istream& input);
