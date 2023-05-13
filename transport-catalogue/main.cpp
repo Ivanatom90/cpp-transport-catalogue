8cac1b19af0647978606e73a060afea8656c1259
Ivanatom90 <115657086+Ivanatom90@users.noreply.github.com>
2023-05-14 00:45:34 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -2,15 +2,16 @@
 #include <fstream>
 
 #include "input_reader.h"
-#include "transport_catalogue.h"
+//#include "transport_catalogue.h"
 #include "stat_reader.h"
 
 using namespace std;
 
 int main()
 {
-    RequestBase B = GetRequestBase(cin);
-    TransportCatalog TK(B);
-    RequestOut RB(TK, cin);
+    RequestBase B(cin);
+    RequestOut RB(B.TCatalog_, cin);
+    RB.GetOutPut();
     return 0;
 }
+
