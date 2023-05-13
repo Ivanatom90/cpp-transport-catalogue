7211331cb0c46e468294be7d44a04783f8196e45
Ivanatom90 <115657086+Ivanatom90@users.noreply.github.com>
2023-05-14 00:49:04 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -11,17 +11,12 @@
 
 #include "geo.h"
 
-
-
 struct StopDist{
   StopDist() = default;
   std::string stop1 = "";
   std::string stop2 = "";
   int dist = 0;
 };
-
-
-
 
 struct BusStop{
     BusStop() = default;
@@ -44,18 +39,15 @@ struct BusRoute{
       bool not_empty = false;
 };
 
-
 class TransportCatalog{
     public:
-
-    void AddStop(BusStop& bs);
-    void AddBusRoute(BusRoute& br);
-    void AddDistBetweenStops(std::pair<std::string, std::string>& stop1_stop2, int dist);
-    void AddBusNumberToStop(std::string& stop_name, std::string& bus_number);
-
-    BusStop* GetBusStop(const std::string& stop) const;
-    BusRoute* GetBusRoude(const std::string& bus) const;
-    int GetDistaseBetweenStops(std::pair<std::string, std::string>& stops) const;
+      void AddStop(BusStop& bs);
+      void AddBusRoute(BusRoute& br);
+      void AddDistBetweenStops(std::pair<std::string, std::string>& stop1_stop2, int dist);
+      void AddBusNumberToStop(std::string& stop_name, std::string& bus_number);
+      BusStop* GetBusStop(const std::string& stop) const;
+      BusRoute* GetBusRoude(const std::string& bus) const;
+      int GetDistaseBetweenStops(std::pair<std::string, std::string>& stops) const;
 
         class Hash{
 
@@ -63,11 +55,11 @@ class TransportCatalog{
             size_t operator()(const std::pair<std::string, std::string>&) const;
         };
 
-private:
-    std::deque<BusStop> stops_base_;
-    std::deque<BusRoute> buses_routes_;
-    std::unordered_map<std::pair<std::string, std::string>, int, Hash> distanse_stop_;
-    std::unordered_map<std::string, BusStop*> stops_;
-    std::unordered_map<std::string, BusRoute*> routes_;
+    private:
+      std::deque<BusStop> stops_base_;
+      std::deque<BusRoute> buses_routes_;
+      std::unordered_map<std::pair<std::string, std::string>, int, Hash> distanse_stop_;
+      std::unordered_map<std::string, BusStop*> stops_;
+      std::unordered_map<std::string, BusRoute*> routes_;
 };
 
