eea379b8f098a60dc641f40f14be2b7e25b78f1b
Ivanatom90 <115657086+Ivanatom90@users.noreply.github.com>
2023-11-14 20:39:39 +0300

    Update geo.h
    s11

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -1,26 +1,36 @@
 #pragma once
 
 #include <cmath>
+#include <svg.h>
+#include <numeric>
+#include <algorithm>
+
+namespace transport_catalogue {
+namespace detail {
+namespace geo {
+
+const int EARTH_RADIUS = 6371000;
+const double PI = 3.1415926535;
 
 struct Coordinates {
-    double lat;
-    double lng;
+    Coordinates(double lat, double lon): latitude(lat), longitude(lon){}
+    double latitude;
+    double longitude;
+
     bool operator==(const Coordinates& other) const {
-        return lat == other.lat && lng == other.lng;
+        return latitude == other.latitude && longitude == other.longitude;
     }
     bool operator!=(const Coordinates& other) const {
         return !(*this == other);
     }
 };
 
-inline double ComputeDistance(Coordinates from, Coordinates to) {
-    using namespace std;
-    if (from == to) {
-        return 0;
-    }
-    static const double dr = 3.1415926535 / 180.;
-    static const double earth_radius = 6371000;
-    return acos(sin(from.lat * dr) * sin(to.lat * dr)
-                + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-        * earth_radius;
-}
+double compute_distance(Coordinates start, Coordinates end);
+
+
+
+
+
+}//end namespace geo
+}//end namespace detail
+}//end namespace transport_catalogue
