95986471e414066c5282235d7a3790dfbd5fce27
Ivanatom90 <115657086+Ivanatom90@users.noreply.github.com>
2023-11-26 04:42:27 +0300

    Update request_handler.h

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -1,25 +1,61 @@
 #pragma once
+
+
 #include "transport_catalogue.h"
 #include "map_renderer.h"
-#include "geo.h"
 
 using namespace transport_catalogue;
 using namespace map_renderer;
 
 namespace request_handler {
 
-using BusPoints = std::vector<std::pair<std::string_view, std::vector<geo::Coordinates>>>;
-
 class RequestHandler {
 public:
 
-    RequestHandler(TransportCatalogue& db, const MapRenderer& map_renderer_);
+    RequestHandler(TransportCatalog& tc);
 
-    std::vector<geo::Coordinates> get_stops_coordinates() const;
-    void render_map(std::ostream& out) const;
+    void CreateBusRoutesForMap();
+    std::vector<BusStopForMap> CreateBusRouteForMap(const BusRoute& bus_route);
+    BusRoutes& GetBusesRoutes();
 
 private:
-    TransportCatalogue& db_;
-    const MapRenderer& map_renderer_;
+    TransportCatalog& transport_catalog_;
+    BusRoutes buses_routes_;
+
 };
 }
+/*
+ * Здесь можно было бы разместить код обработчика запросов к базе, содержащего логику, которую не
+ * хотелось бы помещать ни в transport_catalogue, ни в json reader.
+ *
+ * В качестве источника для идей предлагаем взглянуть на нашу версию обработчика запросов.
+ * Вы можете реализовать обработку запросов способом, который удобнее вам.
+ *
+ * Если вы затрудняетесь выбрать, что можно было бы поместить в этот файл,
+ * можете оставить его пустым.
+ */
+
+// Класс RequestHandler играет роль Фасада, упрощающего взаимодействие JSON reader-а
+// с другими подсистемами приложения.
+// См. паттерн проектирования Фасад: https://ru.wikipedia.org/wiki/Фасад_(шаблон_проектирования)
+/*
+class RequestHandler {
+public:
+    // MapRenderer понадобится в следующей части итогового проекта
+    RequestHandler(const TransportCatalogue& db, const renderer::MapRenderer& renderer);
+
+    // Возвращает информацию о маршруте (запрос Bus)
+    std::optional<BusStat> GetBusStat(const std::string_view& bus_name) const;
+
+    // Возвращает маршруты, проходящие через
+    const std::unordered_set<BusPtr>* GetBusesByStop(const std::string_view& stop_name) const;
+
+    // Этот метод будет нужен в следующей части итогового проекта
+    svg::Document RenderMap() const;
+
+private:
+    // RequestHandler использует агрегацию объектов "Транспортный Справочник" и "Визуализатор Карты"
+    const TransportCatalogue& db_;
+    const renderer::MapRenderer& renderer_;
+};
+*/
