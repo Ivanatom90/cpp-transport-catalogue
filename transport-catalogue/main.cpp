16c59734dc3b4934dcfa2735cb8404b689fbbf01
Ivanatom90 <115657086+Ivanatom90@users.noreply.github.com>
2023-11-26 04:40:55 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,17 +1,57 @@
-#include <iostream>
+#include "transport_catalogue.h"
+#include "json_reader.h"
+#include "domain.h"
+#include "map_renderer.h"
+#include "request_handler.h"
 #include <fstream>
+#include <iostream>
+#include <ostream>
+#include <sstream>
 
-#include "input_reader.h"
-//#include "transport_catalogue.h"
-#include "stat_reader.h"
+
 
 using namespace std;
+using namespace transport_catalogue;
+using namespace transport_catalogue::detail;
+using namespace transport_catalogue::detail::json;
 
-int main()
-{
-    RequestBase B(cin);
-    RequestOut RB(B.TCatalog_, cin);
-    RB.GetOutPut();
-    return 0;
+int main() {
+
+    std::ostringstream oss;
+    //std::ofstream picture("out.svg");
+    //std::ofstream text("test_out.txt");
+    //std::ifstream infile("in.txt");
+    Document doc = load(cin);
+    JSONr readJSON(doc, cout);
+    readJSON.ParseReq();
+    readJSON.CreateTransportCataloge();    
+    //TransportCatalog tc = std::move(readJSON.GetTransportCataloge());
+    TransportCatalog tc = readJSON.GetTransportCataloge();
+    request_handler::RequestHandler req_handler(tc);
+    RenderSettings rs = std::move(readJSON.GetRenderSettings());
+    BusRoutes& br = req_handler.GetBusesRoutes();
+    MapRenderer map_renderer(std::move(rs),std::move(br));
+    //map_renderer.PrintDocument(text);
+    //map_renderer.PrintDocument(picture);
+    map_renderer.PrintDocument(oss);
+    std::string mapa;
+    mapa = oss.str();
+    readJSON.SetMap(mapa);
+    readJSON.PrintAnswer();
+
+
+
+    //readJSON.PrintAnswer();
+
+
+
+    /*
+     * Примерная структура программы:
+     *
+     * Считать JSON из stdin
+     * Построить на его основе JSON базу данных транспортного справочника
+     * Выполнить запросы к справочнику, находящиеся в массиве "stat_requests", построив JSON-массив
+     * с ответами.
+     * Вывести в stdout ответы в виде JSON
+     */
 }
-
