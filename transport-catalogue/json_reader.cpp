9847d11c301d1efb38fde6e46ef9d45881049c4d
Ivanatom90 <115657086+Ivanatom90@users.noreply.github.com>
2023-11-27 05:02:55 +0300

    Update json_reader.cpp

--- transport-catalogue/json_reader.cpp
+++ transport-catalogue/json_reader.cpp
@@ -9,8 +9,8 @@ JSONr::JSONr(Document& doc, std::ostream& ou): document_(doc), out(ou) {}
 
 void JSONr::ParseReq(){
     Dict main_node;
-    if (document_.get_root().is_map()){
-        main_node = document_.get_root().as_map();
+    if (document_.GetRoot().IsDict()){
+        main_node = document_.GetRoot().AsDict();
             JSONr::ParseBasReq(main_node.at("base_requests"));
             JSONr::ParseRenderSettings(main_node.at("render_settings"));
             JSONr::ParseStatReq(main_node.at("stat_requests"));
@@ -22,14 +22,14 @@ void JSONr::ParseBasReq(const Node& BasicRegNode){
     std::vector<Node> buses_nodes;
     std::vector<Node> stops_nodes;
 
-    if (BasicRegNode.is_array()){
-            const Array& node_base = BasicRegNode.as_array();
+    if (BasicRegNode.IsArray()){
+            const Array& node_base = BasicRegNode.AsArray();
             for(const Node& node:node_base){
-                if(node.is_map()){
-                    const Dict&  reg_node = node.as_map();
-                    if(reg_node.at("type").as_string() == "Bus"){
+                if(node.IsDict()){
+                    const Dict&  reg_node = node.AsDict();
+                    if(reg_node.at("type").AsString() == "Bus"){
                         buses_nodes.push_back(node);
-                    } else if(reg_node.at("type").as_string() == "Stop"){
+                    } else if(reg_node.at("type").AsString() == "Stop"){
                         stops_nodes.push_back(node);
                     } else {
                         throw "parse node don't have value";
@@ -53,13 +53,13 @@ void JSONr::CreateBusJ(const Node& bus){
     Bus_J bus_js;
     Dict bus_node;
 
-    if(bus.is_map()){
-        bus_node = bus.as_map();
-        bus_js.name = bus_node.at("name").as_string();
-        for(const Node& n:bus_node.at("stops").as_array()){
-            bus_js.route.push_back(n.as_string());
+    if(bus.IsDict()){
+        bus_node = bus.AsDict();
+        bus_js.name = bus_node.at("name").AsString();
+        for(const Node& n:bus_node.at("stops").AsArray()){
+            bus_js.route.push_back(n.AsString());
         }
-        bus_js.is_roudtrip = bus_node.at("is_roundtrip").as_bool();
+        bus_js.is_roudtrip = bus_node.at("is_roundtrip").AsBool();
     } else {
         throw "not create BusJ";
     }
@@ -70,15 +70,15 @@ void JSONr::CreateBusJ(const Node& bus){
 void JSONr::CreateStopJ(const Node& stop){
     Stop_J stop_js;
     Dict stop_node;
-    if(stop.is_map()){
-        stop_node = stop.as_map();
-        stop_js.name = stop_node.at("name").as_string();
-        stop_js.latitude = stop_node.at("latitude").as_double();
-        stop_js.longitude = stop_node.at("longitude").as_double();
-        for(const auto& dist:stop_node.at("road_distances").as_map()  ){
+    if(stop.IsDict()){
+        stop_node = stop.AsDict();
+        stop_js.name = stop_node.at("name").AsString();
+        stop_js.latitude = stop_node.at("latitude").AsDouble();
+        stop_js.longitude = stop_node.at("longitude").AsDouble();
+        for(const auto& dist:stop_node.at("road_distances").AsDict()  ){
 
                 std::string key = dist.first;
-                stop_js.dist_to[dist.first] = dist.second.as_int();
+                stop_js.dist_to[dist.first] = dist.second.AsInt();
 
         }
     stops_j_.push_back(stop_js);
@@ -89,17 +89,17 @@ void JSONr::CreateStopJ(const Node& stop){
 
 void JSONr::ParseStatReq(const Node& BasicRegnode){
     StatRequest sr;
-    if(BasicRegnode.is_array()){
-       for(const Node& node:BasicRegnode.as_array()){
+    if(BasicRegnode.IsArray()){
+       for(const Node& node:BasicRegnode.AsArray()){
            try{
-               const Dict& dict = node.as_map();
-               if(dict.at("type").as_string() != "Map"){
-                   sr.id = dict.at("id").as_int();
-                   sr.name = dict.at("name").as_string();
-                   sr.type = dict.at("type").as_string();
+               const Dict& dict = node.AsDict();
+               if(dict.at("type").AsString() != "Map"){
+                   sr.id = dict.at("id").AsInt();
+                   sr.name = dict.at("name").AsString();
+                   sr.type = dict.at("type").AsString();
                } else {
-                   sr.id = dict.at("id").as_int();
-                   sr.type = dict.at("type").as_string();
+                   sr.id = dict.at("id").AsInt();
+                   sr.type = dict.at("type").AsString();
                }
 
                stat_reqs_.push_back(sr);
@@ -115,35 +115,35 @@ void JSONr::ParseStatReq(const Node& BasicRegnode){
 
 
 svg::Color JSONr::DetermineColor(const Node& color){
-    if(color.is_array()){
-        if(color.as_array().size() == 4){
-            const Array& rgba = color.as_array();
-            return svg::Rgba(rgba[0].as_int(), rgba[1].as_int(),rgba[2].as_int(), rgba[3].as_double());
-        } else if(color.as_array().size() == 3){
-            const Array& rgb = color.as_array();
-            return svg::Rgb(rgb[0].as_int(), rgb[1].as_int(),rgb[2].as_int());
+    if(color.IsArray()){
+        if(color.AsArray().size() == 4){
+            const Array& rgba = color.AsArray();
+            return svg::Rgba(rgba[0].AsInt(), rgba[1].AsInt(),rgba[2].AsInt(), rgba[3].AsDouble());
+        } else if(color.AsArray().size() == 3){
+            const Array& rgb = color.AsArray();
+            return svg::Rgb(rgb[0].AsInt(), rgb[1].AsInt(),rgb[2].AsInt());
         }
     }
-        return color.as_string();
+        return color.AsString();
 
 }
 
 void JSONr::ParseRenderSettings(const Node& BasicRegnode){
     RenderSettings& rs = render_settings_;
-    if(BasicRegnode.is_map()){
-            const Dict& dict = BasicRegnode.as_map();
-            rs.width = dict.at("width").as_double();
-            rs.height = dict.at("height").as_double();
-            rs.padding = dict.at("padding").as_double();
-            rs.stop_radius = dict.at("stop_radius").as_double();
-            rs.line_width = dict.at("line_width").as_double();
-            rs.bus_label_font_size = dict.at("bus_label_font_size").as_int();
-            rs.bus_label_offset = {dict.at("bus_label_offset").as_array()[0].as_double(),dict.at("bus_label_offset").as_array()[1].as_double()} ;
-            rs.stop_label_offset = {dict.at("stop_label_offset").as_array()[0].as_double(),dict.at("stop_label_offset").as_array()[1].as_double()};
-            rs.underlayer_width = dict.at("underlayer_width").as_double();
+    if(BasicRegnode.IsDict()){
+            const Dict& dict = BasicRegnode.AsDict();
+            rs.width = dict.at("width").AsDouble();
+            rs.height = dict.at("height").AsDouble();
+            rs.padding = dict.at("padding").AsDouble();
+            rs.stop_radius = dict.at("stop_radius").AsDouble();
+            rs.line_width = dict.at("line_width").AsDouble();
+            rs.bus_label_font_size = dict.at("bus_label_font_size").AsInt();
+            rs.bus_label_offset = {dict.at("bus_label_offset").AsArray()[0].AsDouble(),dict.at("bus_label_offset").AsArray()[1].AsDouble()} ;
+            rs.stop_label_offset = {dict.at("stop_label_offset").AsArray()[0].AsDouble(),dict.at("stop_label_offset").AsArray()[1].AsDouble()};
+            rs.underlayer_width = dict.at("underlayer_width").AsDouble();
             rs.underlayer_color = DetermineColor(dict.at("underlayer_color"));
-            rs.stop_label_font_size = dict.at("stop_label_font_size").as_int();
-            const Array& palette = dict.at("color_palette").as_array();
+            rs.stop_label_font_size = dict.at("stop_label_font_size").AsInt();
+            const Array& palette = dict.at("color_palette").AsArray();
             for(const Node& p : palette){
                 rs.color_palette.push_back(DetermineColor(p));
             }
@@ -176,40 +176,148 @@ void JSONr::CreateTransportCataloge(){
     }
 }
 
-void JSONr::CreateAnswer(){
 
+
+void JSONr::CreateAnswer(){
+    Builder answer_build;
+    answer_build.StartArray();
     for(const StatRequest& sr:stat_reqs_){
-        Answer answer;
+        //Answer answer;
+
+        Dict data;
+        if(sr.type == "Bus"){
+
+             if(transport_catalogue_.BusRoudeExist(sr.name)){
+                const BusRoute* br =transport_catalogue_.GetBusRoude(sr.name);
+
+
+                data["curvature"] = br->curvature;
+                data["request_id"] =sr.id;
+                data["route_length"] = int(br->distance);
+                data["unique_stop_count"] = br->unique_route_number;
+
+                (br->is_roundtrip)
+                        ? data["stop_count"]=int(br->route.size())
+                        : data["stop_count"]= ((int(br->route.size())*2)-1);
+
+                         answer_build.Value(data);
+
+             } else {
+                         data["request_id"] = sr.id;
+                         data["error_message"] = "not found";
+
+                         answer_build.Value(data);
+                }
+
+        } else if(sr.type == "Stop"){
+
+
+                data["request_id"] = sr.id;
+                if(transport_catalogue_.BusStopExist(sr.name)){
+                Array buses;
+                const BusStop* bs = transport_catalogue_.GetBusStop(sr.name);
+
+                for(const std::string& bus_number:bs->buses_numbers){
+                    buses.push_back(bus_number);
+
+                }
+                data["buses"] = buses;
+                answer_build.Value(data);
+            } else {
+                  data["error_message"] = "not found";
+                  answer_build.Value(data);
+                //answer["error_message"] = "not found";
+            }
+        } else if(sr.type == "Map"){
+
+                      data["request_id"] = sr.id;
+                      std::string str = "";
+                for(const char ch:mapa_){
+                    if(ch == '"' || ch == '\\'){
+                        str += '\\';
+                    }
+                    str +=ch;
+                }
+
+                data["map"] = mapa_;
+                answer_build.Value(data);
+                //answer["map"] = str;
+        }
+        else {
+            throw "no answer";
+        }
+
+      //answers_.push_back(answer);
+    }
+    answer_build.EndArray();
+   Document doc(answer_build.Build());
+   Print(doc, out);
+
+}
+
+
+
+
+
+
+/*
+void JSONr::CreateAnswer(){
+    Builder answer_build;
+    answer_build.StartArray();
+    for(const StatRequest& sr:stat_reqs_){
+        //Answer answer;
+
 
         if(sr.type == "Bus"){
 
              if(transport_catalogue_.BusRoudeExist(sr.name)){
                 const BusRoute* br =transport_catalogue_.GetBusRoude(sr.name);
-                answer["curvature"] = br->curvature;
-                answer["request_id"] = sr.id;
-                answer["route_length"] = int(br->distance);
-                answer["unique_stop_count"] = br->unique_route_number;
+
+                answer_build.StartDict()
+                        .Key("curvature").Value(br->curvature)
+                        .Key("request_id").Value(sr.id)
+                        .Key("route_length").Value(int(br->distance))
+                        .Key("unique_stop_count").Value(br->unique_route_number);
+
                 (br->is_roundtrip)
-                    ? answer["stop_count"] = int(br->route.size())
-                    : answer["stop_count"] = (int(br->route.size())*2)-1;
+                        ? answer_build.Key("stop_count").Value(int(br->route.size()))
+                        : answer_build.Key("stop_count").Value((int(br->route.size())*2)-1);
+                answer_build.EndDict();
+
+
+
              } else {
-                 answer["request_id"] = sr.id;
-                 answer["error_message"] = "not found";
-             }
+
+                 answer_build.StartDict()
+                         .Key("request_id").Value(sr.id)
+                         .Key("error_message").Value("not found")
+                         .EndDict();
+                }
+
         } else if(sr.type == "Stop"){
-            answer["request_id"] = sr.id;
+
+            answer_build.StartDict()
+                    .Key("request_id").Value(sr.id);
+            //answer["request_id"] = sr.id;
             if(transport_catalogue_.BusStopExist(sr.name)){
-                std::vector<std::string> buses;
+                Array buses;
                 const BusStop* bs = transport_catalogue_.GetBusStop(sr.name);
+
                 for(const std::string& bus_number:bs->buses_numbers){
                     buses.push_back(bus_number);
+
                 }
-                answer["buses"]=buses;
+                answer_build.Key("buses").Value(buses).EndDict();
+                //answer["buses"]=buses;
             } else {
-                answer["error_message"] = "not found";
+                answer_build
+                        .Key("error_message").Value("not found").EndDict();
+                //answer["error_message"] = "not found";
             }
         } else if(sr.type == "Map"){
-                answer["request_id"] =sr.id;
+              answer_build.StartDict()
+                      .Key("request_id").Value(sr.id);
+            //answer["request_id"] =sr.id;
                 std::string str = "";
                 for(const char ch:mapa_){
                     if(ch == '"' || ch == '\\'){
@@ -217,16 +325,27 @@ void JSONr::CreateAnswer(){
                     }
                     str +=ch;
                 }
-                answer["map"] = str;
+
+                answer_build
+                        .Key("map").Value(mapa_).EndDict();
+                //answer["map"] = str;
         }
         else {
             throw "no answer";
         }
 
-      answers_.push_back(answer);
+      //answers_.push_back(answer);
     }
+    answer_build.EndArray();
+   Document doc(answer_build.Build());
+   Print(doc, out);
 
 }
+
+
+
+
+
 
 struct AnswerPrinter{
     void operator()(std::string str){
@@ -275,7 +394,7 @@ void JSONr::PrintAnswer(){
     out<<'\n'<<"  ]"<<std::endl;
 }
 
-
+*/
 TransportCatalog& JSONr::GetTransportCataloge(){
     return transport_catalogue_;
 }
@@ -284,75 +403,6 @@ RenderSettings& JSONr::GetRenderSettings(){
     return render_settings_;
 }
 
-/*
-void JSONr::CreateAnsvertoReq(){
-    out<<'['<<'\n';
-    size_t size_req = stat_reqs_.size();
-    for(const StatRequest& sr:stat_reqs_){
-        if(sr.type == "Bus"){
-            AnswertoRequestBus arb;
-            if(!transport_catalogue_.BusRoudeExist(sr.name)){
-                out<<"  {"<<'\n'<<R"(    "request_id": )"<<sr.id<<","<<'\n'
-                  << R"(    "error_message": )"<< R"("not found")"<<'\n'
-                  <<"  }"<<std::endl;
-            } else {
-            const BusRoute* br = transport_catalogue_.GetBusRoude(sr.name);
-                arb.curvature = br->curvature;
-                arb.request_id = sr.id;
-                arb.route_length = br->distance;
-                arb.stop_count = br->route.size()+1;
-                arb.unique_stop_count = br->unique_route_number;
-                answer_bus_.push_back(arb);
-                out<<arb;
-                  }
-        } else if(sr.type == "Stop"){
-            if(!transport_catalogue_.BusStopExist(sr.name)){
-                out<<"  {"<<'\n'<<R"(    "request_id": )"<<sr.id<<","<<'\n'
-                  << R"(    "error_message": )"<< R"("not found")"<<'\n'
-                  <<"  }"<<std::endl;
-            } else {
-
-            AnswertoRequestStop ars;
-            const BusStop* bs = transport_catalogue_.GetBusStop(sr.name);
-
-                ars.request_id = sr.id;
-                ars.buses_names = bs->buses_numbers;
-            out<<ars;
-        }
-        } else {
-            throw "no answer";
-        }
-        if(--size_req !=0){out<<", ";}
-
-    }
-    out<<']'<<std::endl;
-}
-
-std::ostream& operator<<(std::ostream& ou, AnswertoRequestBus& arb){
-    ou<<"  {"<<'\n';
-    ou<<R"(    "curvature": )"<<arb.curvature<<","<<'\n'
-     <<R"(    "request_id": )"<<arb.request_id<<","<<'\n'
-    <<R"(    "route_length": )" <<arb.route_length<<","<<'\n'
-    <<R"(    "stop_count": )"<<arb.stop_count<<","<<'\n'
-    <<R"(    "unique_stop_count": )"<<arb.unique_stop_count<<'\n';
-    ou<<"  }"<<std::endl;
-    return ou;
-}
-
-std::ostream& operator<<(std::ostream& ou, AnswertoRequestStop& ars){
-    ou<<"  {"<<'\n';
-        ou<<R"(    "buses": [)"<<'\n';
-        bool first = true;
-        for(const std::string& bus:ars.buses_names){
-            (first)? ou<<"    "<<bus : ou<<", "<<'\n'<<"    "<<bus;
-            first = false;
-        }
-        ou<<'\n'<<"],"<<'\n';
-        ou<<R"(    "request_id": )"<<ars.request_id<<'\n';
-    ou<<"  }"<<std::endl;
-    return ou;
-}
-*/
 
 }//end namespace json
 }//end namespace detail
