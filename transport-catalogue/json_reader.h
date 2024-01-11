951f6c9c4ea820077671a2d37c67753fa8a5dc20
Ivanatom90 <115657086+Ivanatom90@users.noreply.github.com>
2024-01-12 00:12:34 +0300

    Update json_reader.h

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -9,6 +9,10 @@
 #include <iostream>
 #include <ostream>
 #include "json_builder.h"
+#include "transport_router.h"
+#include "graph.h"
+
+#include "log_duration.h"
 
 
 namespace transport_catalogue {
@@ -22,11 +26,14 @@ using  map_renderer::RenderSettings;
 
 using Answer = std::map<std::string, std::variant<std::string, int, double, std::vector<std::string>>>;
 
+using transport_catalogue::detail::router::TransportRouter;
+
 struct StatRequest {
     int id;
     std::string type;
     std::string name;
-
+    std::optional<std::string> from;
+    std::optional<std::string> to;
 };
 
 struct Bus_J{
@@ -47,6 +54,7 @@ struct Stop_J{
 
 
 
+
 class JSONr{
 public:
 
@@ -59,32 +67,41 @@ public:
     void ParseBasReq(const Node& BasicRegnode);
     void ParseRenderSettings(const Node& BasicRegnode);
     void ParseStatReq(const Node& BasicRegnode);
+    void ParseRoutingSettings(const Node& BasicRegnode);
     void CreateBusJ(const Node& bus);
     void CreateStopJ(const Node& stop);
 
     void CreateTransportCataloge();
 
     void CreateAnswer();
+    Array CreateAnsRoute(Dict& dict);
     void PrintAnswer();
 
     void SetMap(std::string mapa){
         mapa_ = mapa;
     }
 
+    void SetTransportRouter(std::unique_ptr<TransportRouter> tr);
+
     TransportCatalog& GetTransportCataloge();
     RenderSettings& GetRenderSettings();
+
 
 
 
 private:
     json::Document& document_;
     TransportCatalog transport_catalogue_;
+    std::unique_ptr<TransportRouter> transport_router_;
+    std::unique_ptr<graph::Router<double>> router_ptr_;
     std::vector<Bus_J> buses_j_;
     std::vector<Stop_J> stops_j_;
     std::vector<StatRequest> stat_reqs_;
     std::vector<Answer> answers_;
     std::string mapa_;
     RenderSettings render_settings_;
+
+    RoutingSettings route_settings_;
 
     std::ostream& out;
 
