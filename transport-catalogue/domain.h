90c895cd28023cf6bad3a59eb983f9175bac9745
Ivanatom90 <115657086+Ivanatom90@users.noreply.github.com>
2024-01-12 00:09:41 +0300

    Update domain.h

--- transport-catalogue/domain.h
+++ transport-catalogue/domain.h
@@ -1,14 +1,37 @@
 #pragma once
 #include <iostream>
-#include "transport_catalogue.h"
 #include <algorithm>
 #include <vector>
+#include <variant>
+
+
+#include "transport_catalogue.h"
+#include "graph.h"
 
 namespace transport_catalogue {
 
 
 
+struct StopEdge {
+    std::string_view name;
+    double time = 0;
+};
 
+struct BusEdge {
+    std::string_view bus_name;
+    size_t span_count = 0;
+    double time = 0;
+};
+
+struct RouterByStop {
+    graph::VertexId bus_wait_start;
+    graph::VertexId bus_wait_end;
+};
+
+struct RouteInfo {
+    double total_time = 0.;
+    std::vector<std::variant<StopEdge, BusEdge>> edges;
+};
 namespace detail {
 namespace bus {
 
